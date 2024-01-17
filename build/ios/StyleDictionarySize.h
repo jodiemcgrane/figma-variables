0db796461f8a8ffc7f897d3cb7147a2ad3da63f5
Jodie Mcgrane <jodiemcgrane101@gmail.com>
2024-01-17 11:16:25 +0000

    Initial commit

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,17 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Mon, 08 May 2023 07:37:37 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const TypographyBodyH3FontWeight;
+extern float const TypographyBodyH4StrikeThroughFontWeight;
+extern float const TypographyBodyItalicFontWeight;
+extern float const TypographyBodyExtraBoldCondensedItalicFontWeight;
+extern float const TypographyBodyMediumExtendedItalicFontWeight;
+extern float const TypographyBodySuperFontWeight;
